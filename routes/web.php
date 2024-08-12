1e1f0bf3881c1228a981c2d48fce9decb9a87412
Saeed <78552486+appsaeed@users.noreply.github.com>
2024-08-12 16:24:02 +0600

    create fresh sources

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,7 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    $data = json_decode(file_get_contents(public_path('data.json')), false);
-    return view('welcome', compact('data'));
+    return view('welcome');
 });
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-require __DIR__ . '/auth.php';
